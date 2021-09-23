a2833c54fbedde1da2f6ad2b1e805554c5eeedf9
Viktor <72042504+trallan@users.noreply.github.com>
2021-09-23 23:39:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let characterName;
 let characterGender;
 let characterClass;
 let newPlayer;
+let test;
 
 class Player{
-    constructor(name, gender, role, health, level, exp, attack){
+    constructor(name, gender, role, health, level, exp, attack, attackrate){
         this.name = name;
         this.gender = gender;
         this.role = role;
@@ -13,8 +14,25 @@ class Player{
         this.level = level;
         this.exp = exp;
         this.attack = attack;
+        this.attackrate = attackrate;
     }
 }
+
+class Enemy{
+    constructor(name, health, exp, attack){
+        this.name = name;
+        this.health = health;
+        this.exp = exp;
+        this.attack = attack;
+    }
+}
+
+let snake = new Enemy('Snake', 50, 5, 20)
+let greywolf = new Enemy('Grey Wolf', 70, 15, 30)
+let pineman = new Enemy('Pineman', 120, 35, 40)
+let bear = new Enemy('Bear', 300, 45, 50)
+let pinetroll = new Enemy('Pinetroll', 210, 65, 50)
+let skeletonmage = new Enemy('Skeleton mage', 230, 75, 80)
 
 let createCharacterPanel = () => {
     game.innerHTML += `
@@ -59,6 +77,20 @@ let showCharacterScreen = () => {
     `
 }
 
+let showEnemyScreen = () => {
+    game.innerHTML += `
+        <div class="enemy-screen">
+            <h2>Choose your enemy</h2>
+            <div class="enemy-name" id="snake" value="snake">${snake.name}</div>
+            <div class="enemy-name" id="greywolf" value="greywolf">${greywolf.name}</div>
+            <div class="enemy-name" id="pineman" value="pineman">${pineman.name}</div>
+            <div class="enemy-name" id="bear" value="bear">${bear.name}</div>
+            <div class="enemy-name" id="pinetroll" value="pinetroll">${pinetroll.name}</div>
+            <div class="enemy-name" id="skeleton-mage" value="skeletonmage">${skeletonmage.name}</div>
+        </div>
+    `
+}
+
 
 let createCharacter = () => {
     let characterPanel = document.querySelector(".create-character-panel");
@@ -94,15 +126,25 @@ let createCharacter = () => {
         characterClass = "Mage";
     }
     if(characterClass == "Knight"){
-        newPlayer = new Player(characterName, characterGender, characterClass, 250, 1, 0, 5)
+        newPlayer = new Player(characterName, characterGender, characterClass, 250, 1, 0, 40, 2)
     }else if(characterClass == "Archer"){
-        newPlayer = new Player(characterName, characterGender, characterClass, 150, 1, 0, 10)
+        newPlayer = new Player(characterName, characterGender, characterClass, 150, 1, 0, 50, 2)
     }else if(characterClass == "Mage"){
-        newPlayer = new Player(characterName, characterGender, characterClass, 100, 1, 0, 15)
+        newPlayer = new Player(characterName, characterGender, characterClass, 100, 1, 0, 65, 2)
     }
     characterPanel.remove();
     showCharacterScreen();
+    showEnemyScreen();
+    battleEnemy();
 }
+
+let battleEnemy = () => {
+    console.log(Math.floor(Math.random() * snake.attack) + 5);
+    let max = newPlayer.attack * newPlayer.attackrate;
+    let damage = (Math.floor(Math.random() * max) + newPlayer.attack)
+    console.log(damage)
+}
+
 
 
 createCharacterPanel();
