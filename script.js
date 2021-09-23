b772e05e733ecf196a794ee09aabf61d106538d5
Viktor <72042504+trallan@users.noreply.github.com>
2021-09-23 16:15:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+let game = document.querySelector('.game');
+let characterName;
+let characterGender;
+let characterClass;
+let newPlayer;
+
+class Player{
+    constructor(name, gender, role, health, attack){
+        this.name = name;
+        this.gender = gender;
+        this.role = role;
+        this.health = health;
+        this.attack = attack;
+    }
+}
+
+let createCharacterPanel = () => {
+    game.innerHTML += `
+    <div class="create-character-panel">
+        <h2>Create a character</h2>
+        <div class="name">
+            <p>Choose a name</p>
+            <input type="text" id="character-name">
+        </div>
+        <div class="gender">
+            <p>Choose a gender</p>
+            <input type="checkbox" id="male-check" value="male">
+            <label for="male-check">Male</label>
+            <input type="checkbox" id="female-check" value="female">
+            <label for="male-check">Female</label>
+        </div>
+        <div class="class">
+            <p>Choose a class</p>
+            <input type="checkbox" class="class-check" id="knight-check" value="knight">
+            <label for="class-check">Knight</label>
+            <input type="checkbox" class="class-check" id="archer-check" value="Archer">
+            <label for="class-check">Archer</label>
+            <input type="checkbox" class="class-check" id="mage-check" value="Mage">
+            <label for="class-check">Mage</label>
+        </div>
+        <button id="create-ch-btn" onclick="createCharacter()">Create character</button>
+    </div>
+    `
+}
+
+let showCharacterScreen = () => {
+    game.innerHTML += `
+        <div class="character-screen">
+            <div id="ch-name">${newPlayer.name}</div>
+            <div id="ch-gender">${newPlayer.gender}</div>
+            <div id="ch-role">${newPlayer.role}</div>
+            <div id="ch-health">${newPlayer.health}</div>
+            <div id="ch-attack">${newPlayer.attack}</div>
+        </div>
+    `
+}
+
+
+let createCharacter = () => {
+    let characterPanel = document.querySelector(".create-character-panel");
+    let characterNameVal = document.querySelector('#character-name').value
+    let maleCheck = document.querySelector('#male-check').checked;
+    let femaleCheck = document.querySelector('#female-check').checked;
+    if(maleCheck == true && femaleCheck == true){
+        return
+    }
+
+    let knightCheck = document.querySelector('#knight-check').checked;
+    let archerCheck = document.querySelector('#archer-check').checked;
+    let mageCheck = document.querySelector('#mage-check').checked;
+    if(knightCheck == true && mageCheck == true && archerCheck == true
+        || maleCheck == false && femaleCheck == false
+        || knightCheck == false && mageCheck == true && archerCheck == true 
+        || knightCheck == true && mageCheck == false && archerCheck == true 
+        || knightCheck == true && mageCheck == true && archerCheck == false
+        || knightCheck == true && mageCheck == true && archerCheck == true){
+        return
+    }
+    characterName = characterNameVal;
+    if(maleCheck == true){
+        characterGender = "Male";
+    }else if(femaleCheck == true){
+        characterGender = "Female";
+    }
+    if(knightCheck == true){
+        characterClass = "Knight";
+    }else if(archerCheck == true){
+        characterClass = "Archer";
+    }else if(mageCheck == true){
+        characterClass = "Mage";
+    }
+    if(characterClass == "Knight"){
+        newPlayer = new Player(characterName, characterGender, characterClass, 250, 5)
+    }else if(characterClass == "Archer"){
+        newPlayer = new Player(characterName, characterGender, characterClass, 150, 10)
+    }else if(characterClass == "Mage"){
+        newPlayer = new Player(characterName, characterGender, characterClass, 100, 15)
+    }
+    characterPanel.remove();
+    showCharacterScreen();
+}
+
+
+createCharacterPanel();
+
